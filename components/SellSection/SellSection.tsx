027e297b997dcedf9d304d0fb6d6f5342430e208
Joydeep Singha <67229156+Jds-23@users.noreply.github.com>
2021-06-20 20:38:42 +0530

    add: stepper

--- components/SellSection/SellSection.tsx
+++ components/SellSection/SellSection.tsx
@@ -5,6 +5,7 @@ import SwapCurrencyInputBox from "../SwapCurrencyInputBox/SwapCurrencyInputBox";
 import Main from "../Main/Main";
 import { useWalletAddress } from "../../context/StateProvider";
 import { QuestionOutlineIcon } from "@chakra-ui/icons";
+import ProgressStepper from "../ProgressStepper/ProgressStepper";
 
 interface MyProps {
   onOpen: () => void;
@@ -15,6 +16,7 @@ const SellSection: React.FC<React.HTMLAttributes<HTMLDivElement> & MyProps> = ({
   const [amount, setAmount] = useState("");
   const [balance, setBalance] = useState("");
   const [currency, setCurrency] = useState("bnb");
+  const [state2, setState2] = useState(0);
 
   const [fromCurrency, setFromCurrency] = useState("GAINPROTOCOL");
   const [toCurrency, setToCurrency] = useState("BNB");
@@ -113,9 +115,41 @@ const SellSection: React.FC<React.HTMLAttributes<HTMLDivElement> & MyProps> = ({
           <p>Slippage Tolerance</p>
           <p>12%</p>
         </div>
-        <CustomButton onClick={onOpen} block>
-          {address === "" ? "Unlock Wallet" : "Swap"}
-        </CustomButton>
+        <div style={{ display: "flex", gap: "5px", width: "100%" }}>
+          <CustomButton
+            onClick={() => setState2(1)}
+            style={{
+              borderRadius: "14px",
+              width: "40%",
+              fontSize: "12px",
+              lineHeight: "13px",
+            }}
+            disabled={state2 > 0}
+          >
+            {"Approve Gain Protocol"}
+          </CustomButton>
+          <CustomButton
+            onClick={() => setState2(2)}
+            style={{
+              borderRadius: "14px",
+              width: "60%",
+              fontSize: "12px",
+              lineHeight: "13px",
+            }}
+            disabled={state2 !== 1}
+          >
+            {"Swap"}
+          </CustomButton>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "10px",
+          }}
+        >
+          <ProgressStepper state={state2} setState={setState2} />
+        </div>
       </Main>
     </div>
   );
