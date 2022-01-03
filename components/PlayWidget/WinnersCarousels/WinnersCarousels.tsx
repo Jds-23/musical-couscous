5512ffdbe601e3c4843c4db43cd5f25b9107c68a
matanelgabsi <matan.elgabsi@gmail.com>
2022-01-03 18:58:05 +0200

    Bugfix

--- components/PlayWidget/WinnersCarousels/WinnersCarousels.tsx
+++ components/PlayWidget/WinnersCarousels/WinnersCarousels.tsx
@@ -1,16 +1,20 @@
+import { BigNumber } from "ethers";
 import { useContext } from "react";
 import { ExternalStateContext } from "../../../context/ExternalState";
 import { formatGain } from "../../../utils";
 import styles from "./WinnersCarousels.module.css";
 const WinnersCarousels = () => {
   const { state: externalState } = useContext(ExternalStateContext);
+  const index = externalState.types?.map((i) => i.toNumber()).indexOf(2);
   return (
     <div className={styles.winnersCarousels}>
       <div>
         <p className={styles.address}>{externalState.winners?.[2] || ""}</p>
         <p className={styles.won__text}>
           WON{" "}
-          {externalState.amounts ? formatGain(externalState.amounts[2], 2) : ""}{" "}
+          {externalState.amounts && index !== undefined && index >= 0
+            ? formatGain(externalState.amounts[index], 2)
+            : ""}{" "}
           GAIN
         </p>
       </div>
